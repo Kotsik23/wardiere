2db232908be5abd516481d82d8248977c041da23
oleg <olegsmushko@gmail.com>
2023-12-11 21:50:43 +0300

    Add manual chunk splitting

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,49 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import { visualizer } from "rollup-plugin-visualizer"
+import { defineConfig, PluginOption } from "vite"
+import { splitVendorChunkPlugin } from "vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [react(), splitVendorChunkPlugin(), visualizer() as PluginOption],
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
 			"@convex": path.resolve(__dirname, "./convex"),
+		},
+	},
+	build: {
+		rollupOptions: {
+			output: {
+				manualChunks: (id: string) => {
+					if (id.includes("@radix-ui") || id.includes("@floating-ui")) {
+						return "@radix-ui"
+					}
+					if (
+						id.includes("react-router-dom") ||
+						id.includes("@remix-run") ||
+						id.includes("react-router")
+					) {
+						return "@react-router"
+					}
+					if (id.includes("tailwind-merge")) {
+						return "tailwind-merge"
+					}
+					if (id.includes("convex")) {
+						return "convex"
+					}
+					if (id.includes("lucide-react")) {
+						return "lucide-react"
+					}
+					if (id.includes("use-query-params") || id.includes("serialize-query-params")) {
+						return "use-query-params"
+					}
+					if (id.includes("sonner")) {
+						return "sonner"
+					}
+				},
+			},
 		},
 	},
 })
