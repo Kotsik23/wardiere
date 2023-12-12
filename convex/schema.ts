7c510864410885bf5a7a8e185ed9123167436588
oleg <olegsmushko@gmail.com>
2023-12-12 11:40:24 +0300

    Add categories table and query - Convex

--- convex/schema.ts
+++ convex/schema.ts
@@ -2,8 +2,8 @@ import { defineSchema, defineTable } from "convex/server"
 import { v } from "convex/values"
 
 export default defineSchema({
-	tasks: defineTable({
-		text: v.string(),
-		isCompleted: v.boolean(),
+	categories: defineTable({
+		name: v.string(),
+		slug: v.string(),
 	}),
 })
