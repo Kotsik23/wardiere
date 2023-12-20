5724b6d416ac3ff3d57fa450f76d02a029651dc8
oleg <olegsmushko@gmail.com>
2023-12-20 16:59:57 +0300

    Change comment button feature

--- src/features/author/comment-author/ui/comment-button.tsx
+++ src/features/author/comment-author/ui/comment-button.tsx
@@ -1,50 +1,20 @@
-import { ArrowRightIcon, MessageSquareIcon } from "lucide-react"
+import { MessageSquareIcon } from "lucide-react"
 import { Link } from "react-router-dom"
-import { useBoolean } from "usehooks-ts"
 import { Doc } from "@convex/_generated/dataModel"
 import { ROUTES } from "@/shared/constants/routes.ts"
 import { Button } from "@/shared/ui/button.tsx"
-import {
-	Dialog,
-	DialogContent,
-	DialogDescription,
-	DialogFooter,
-	DialogHeader,
-	DialogTitle,
-	DialogTrigger,
-} from "@/shared/ui/dialog.tsx"
-import { CreateCommentForm } from "./create-comment-form.tsx"
 
 type CommentButtonProps = {
 	author: Doc<"author">
 }
 
 export const CommentButton = ({ author }: CommentButtonProps) => {
-	const { value: open, setValue: onOpenChange, setFalse: onClose } = useBoolean()
-
 	return (
-		<Dialog open={open} onOpenChange={onOpenChange}>
-			<DialogTrigger asChild>
-				<Button variant={"outline"} className={"flex h-fit items-center gap-2 py-1"}>
-					<MessageSquareIcon className={"h-5 w-5 shrink-0"} />
-					<span className={"pb-1 text-base leading-none"}>{author.comments.length}</span>
-				</Button>
-			</DialogTrigger>
-			<DialogContent>
-				<DialogHeader>
-					<DialogTitle className={"text-2xl"}>Comment Author</DialogTitle>
-					<DialogDescription>What do you think?</DialogDescription>
-				</DialogHeader>
-				<CreateCommentForm authorId={author._id} afterSubmit={onClose} />
-				<DialogFooter>
-					<Button variant={"outline"} asChild>
-						<Link to={ROUTES.AUTHOR(author._id)}>
-							Go to author
-							<ArrowRightIcon className={"ml-2 h-5 w-5"} />
-						</Link>
-					</Button>
-				</DialogFooter>
-			</DialogContent>
-		</Dialog>
+		<Button variant={"outline"} className={"flex h-fit items-center gap-2 py-1"} asChild>
+			<Link to={ROUTES.AUTHOR(author._id)}>
+				<MessageSquareIcon className={"h-5 w-5 shrink-0"} />
+				<span className={"pb-1 text-base leading-none"}>{author.comments.length}</span>
+			</Link>
+		</Button>
 	)
 }
--- src/shared/ui/textarea.tsx
+++ src/shared/ui/textarea.tsx
@@ -10,7 +10,7 @@ const Textarea = React.forwardRef<
 		<TextareaAutosize
 			className={cn(
 				[
-					"flex min-h-fit w-full rounded-md border border-input bg-background px-3 py-2 text-sm transition-all",
+					"flex min-h-[2.5rem] w-full rounded-md border border-input bg-background px-3 py-2 text-sm transition-all",
 					"placeholder:text-muted-foreground",
 					"ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2",
 					"disabled:cursor-not-allowed disabled:opacity-50",
