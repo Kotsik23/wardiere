3b84927ad926643309f33a4ca1c9b7c274523b20
oleg <olegsmushko@gmail.com>
2024-01-05 16:17:01 +0300

    Add load more logic in portfolios list

--- src/widgets/author/ui/portfolios-list.tsx
+++ src/widgets/author/ui/portfolios-list.tsx
@@ -1,6 +1,8 @@
+import { ChevronDownIcon, GalleryVerticalEndIcon } from "lucide-react"
 import { StringParam, useQueryParam, withDefault } from "use-query-params"
 import { Id } from "@convex/_generated/dataModel"
 import { PortfolioImage, PortfoliosWrapper } from "@/entities/author"
+import { Button } from "@/shared/ui/button.tsx"
 import { usePortfoliosList } from "../model/use-portfolios-list.ts"
 import { PortfolioActions } from "./portfolio-actions.tsx"
 
@@ -15,23 +17,45 @@ export const PortfoliosList = ({ authorId, editable = false }: Props) => {
 
 	const { query } = usePortfoliosList({ authorId, categoryId: categoryId as Id<"categories"> })
 
+	if (!query.isLoading && query.results.length <= 0) {
+		return (
+			<div
+				className={"flex h-96 flex-col items-center justify-center gap-6 text-muted-foreground"}
+			>
+				<GalleryVerticalEndIcon className={"size-24"} strokeWidth={1} />
+				<p className={"max-w-sm text-balance text-center text-lg font-medium"}>
+					There is no portfolio images in this category yet
+				</p>
+			</div>
+		)
+	}
+
 	return (
-		<PortfoliosWrapper>
-			{query.results.map(portfolio => (
-				<PortfolioImage
-					key={portfolio._id}
-					portfolioId={portfolio._id}
-					url={portfolio.url}
-					containerClassName={"max-w-full relative group"}
-					actions={
-						<PortfolioActions
-							portfolio={portfolio}
-							editable={editable}
-							triggerClassName={"absolute top-1 right-1"}
-						/>
-					}
-				/>
-			))}
-		</PortfoliosWrapper>
+		<div className={"flex w-full flex-col items-center gap-6"}>
+			<PortfoliosWrapper>
+				{query.results.map(portfolio => (
+					<PortfolioImage
+						key={portfolio._id}
+						portfolioId={portfolio._id}
+						url={portfolio.url}
+						containerClassName={"max-w-full relative group"}
+						actions={
+							<PortfolioActions
+								portfolio={portfolio}
+								editable={editable}
+								triggerClassName={"absolute top-1 right-1"}
+							/>
+						}
+					/>
+				))}
+			</PortfoliosWrapper>
+			<Button
+				variant={"outline"}
+				onClick={() => query.loadMore(10)}
+				disabled={query.status === "Exhausted" || query.isLoading}
+			>
+				Load More <ChevronDownIcon className={"ml-2 size-5"} />
+			</Button>
+		</div>
 	)
 }
